07fe23d206a105a481d272a2c88b1b832a06c304
Ziheng Li <1652961256@qq.com>
2020-12-01 14:38:45 +0800

    refactor: modify the login-register response and fix the case username does not exist.

--- app/register_login.py
+++ app/register_login.py
@@ -10,10 +10,12 @@ def login():
     username = request.form.get('username')
     password = request.form.get('password')
     user = User.query.filter_by(username=username).first()
+    if not user:
+        return 'wrong username', 300
     if user.password == password:
         return 'valid'
     else:
-        return 'invalid'
+        return 'wrong password', 300
 
 
 @register_login.route('/user/register', methods=['POST'])
@@ -22,13 +24,13 @@ def register():
     password = request.form.get('password')
     email = request.form.get('email')
     if User.query.filter_by(username=username).first():
-        return 'username has been taken'
+        return 'username existed', 300
     elif User.query.filter_by(email=email).first():
-        return 'email has been taken'
+        return 'email existed', 300
     else:
         user = User(username=username,
                     password=password,
                     email=email)
         db.session.add(user)
         db.session.commit()
-        return 'user has been established'
+        return 'user established'
