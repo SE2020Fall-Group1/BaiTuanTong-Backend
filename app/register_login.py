c42877cc1ef6de5c3fb2f5f6206bedb166548819
Ziheng Li <1652961256@qq.com>
2020-12-12 15:57:32 +0800

    feat: implement mail captcha.

--- app/register_login.py
+++ app/register_login.py
@@ -25,10 +25,14 @@ def register():
     username = request.form.get('username')
     password = request.form.get('password')
     email = request.form.get('email')
+    captcha = request.form.get('captcha')
+    print(cache)
     if User.query.filter_by(username=username).first():
         return 'username existed', 300
     elif User.query.filter_by(email=email).first():
         return 'email existed', 300
+    elif captcha != cache.get(email):
+        return 'invalid captcha'
     else:
         user = User(username=username,
                     password=password,
@@ -40,16 +44,16 @@ def register():
 
 @register_login.route('/captcha', methods=['GET'])
 def email_captcha():
-    email_address = request.args.get('email_address')
-    if not email_address:
+    email = request.args.get('email')
+    if not email:
         return 'empty address', 400
     captcha = ''.join(map(str, np.random.randint(0, 10, 6)))
-    if cache.get(email_address):
-        return 'click too frequently'
-    msg = Message('百团通注册验证码', recipients=[email_address], body='您的验证码是：%s' % captcha)
+    if cache.get(email):
+        return 'request too frequently'
+    msg = Message('百团通注册验证码', recipients=[email], body='您的验证码是：%s' % captcha)
     try:
         mail.send(msg)
     except smtplib.SMTPException:
         return 'sending failed', 500
-    cache.set(email_address, captcha)
+    cache.set(email, captcha)
     return 'success', 200
