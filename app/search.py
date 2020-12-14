00d424b53f3b21d76d9b128750cddadae3abba77
Ziheng Li <1652961256@qq.com>
2020-12-15 01:47:37 +0800

    bug fix: fix the response of login; replace "request.form" by json.loads; refactor: replace "POST" method in search.py by "GET" and modify the extract_{post,club}_info to make it compatible accord with api;add json.dumps to test requests.

--- app/search.py
+++ app/search.py
@@ -1,5 +1,5 @@
 import json
-from flask import Blueprint, request
+from flask import Blueprint, request, jsonify
 from exts import db
 from .models import User, Club, Post
 search = Blueprint('search', __name__)
@@ -8,38 +8,39 @@ search = Blueprint('search', __name__)
 def extract_club_info(clubs):
     ret_info = []
     for club in clubs:
-        ret_info.append([club.id,
-                         club.club_name,
-                         club.introduction,
-                         User.query.filter_by(id=club.president_id).first().username])
+        ret_info.append({"clubId": club.id,
+                         "clubName": club.club_name,
+                         "introduction": club.introduction,
+                         "president": club.president.username})
     return ret_info
 
 
 def extract_post_info(posts):
     ret_info = []
     for post in posts:
-        ret_info.append([post.id,
-                         post.title,
-                         post.text,
-                         Club.query.filter_by(id=post.club_id).first().club_name])
-    return ret_info
+        ret_info.append({"postId": post.id,
+                         "title": post.title,
+                         "text": post.text,
+                         "clubName": post.club.club_name,
+                         "likeCnt": len(post.likes)})
+    return sorted(ret_info, key=lambda x: x['likeCnt'], reverse=True)
 
 
-@search.route('/club/search', methods=['POST'])
+@search.route('/club/search', methods=['GET'])
 def search_club():
-    keyword = request.form.get('keyword')
+    keyword = request.args.get('keyword')
     clubs = Club.query.filter(Club.club_name.contains(keyword)).all()
-    return json.dumps(extract_club_info(clubs))
+    return jsonify(extract_club_info(clubs))
 
 
-@search.route('/post/search', methods=['POST'])
+@search.route('/post/search', methods=['GET'])
 def search_post():
-    keyword = request.form.get('keyword')
+    keyword = request.args.get('keyword')
     posts = Post.query.filter(Post.title.contains(keyword)).all()
-    return json.dumps(extract_post_info(posts))
+    return jsonify(extract_post_info(posts))
 
 
-@search.route('/post/push', methods=['POST'])
+@search.route('/post/push', methods=['GET'])
 def push_post():
-    posts = Post.query.filter(Post.likes>=0).all()
-    return json.dumps(extract_post_info(posts))
+    posts = Post.query.all()
+    return jsonify(extract_post_info(posts))
