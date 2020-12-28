d5c054325c7e8bb4500f9478a37908d078d10f3e
Ziheng Li <1652961256@qq.com>
2020-12-29 01:45:27 +0800

    fix: fix edit post request.

--- app/image.py
+++ app/image.py
@@ -4,45 +4,6 @@ from .utils import save_image, delete_image
 from exts import db
 image = Blueprint('image', __name__)
 
-#NOTE to be deleted
-#def reset_db():
-#    db.drop_all()
-#    db.create_all()
-#
-#    def add_items():
-#        u1 = User(username='tl', password='hehe', email='tl@pku.edu.cn')
-#        u2 = User(username='dgl', password='gaga', email='dgl@stu.pku.edu.cn')
-#
-#        c1 = Club(club_name='yuanhuo', introduction="yuanhuo introduction", president_id=1)
-#        c2 = Club(club_name='feiying', introduction="feiying introduction", president_id=2)
-#
-#        po1 = Post(title='one', text='jd is too strong', club_id=1)
-#        po2 = Post(title='two', text="let's compliment jd", club_id=1)
-#
-#        u1.followed_clubs.append(c1)
-#        u2.managed_clubs.append(c1)
-#
-#        db.session.add_all([u1, u2, po1, po2, c1, c2])
-#        db.session.commit()
-#
-#    add_items()
-#
-#
-#@image.route('/image/upload', methods=['POST'])
-#def upload_image():
-#    img = request.files.get('img')
-#    rand_name = ''.join(random.sample(string.ascii_letters + string.digits, 16))
-#    img_name = img.filename
-#    url = '/static/images/' + rand_name + img_name
-#    save_image(image)
-#    pic = Picture(url=url)
-#    try:
-#        db.session.add(pic)
-#        db.session.commit()
-#    except:
-#        return 'database error', 500
-#    return 'success', 200
-
 
 @image.route('/user/image/upload', methods=['POST'])
 def upload_user_image():
